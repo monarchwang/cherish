097377159c6cb454267bec4575f0506221909c03
wanggl <wanggl@fenqi.im>
2017-08-04 18:26:59 +0800

    数据源更改为云服务

--- src/main/java/com/monarchwang/website/common/SimpleCORSFilter.java
+++ src/main/java/com/monarchwang/website/common/SimpleCORSFilter.java
@@ -19,7 +19,7 @@ public class SimpleCORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
